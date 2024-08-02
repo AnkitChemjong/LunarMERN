30e9487a57a641b4eeda0fce73147b9d9cf2eba2
Ankit Chemjong <npp.rasik@gmail.com>
2024-08-02 21:50:25 +0545

    h

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root:'src',
+  server:{port:process.env.PORT}
+
 })
--- Server/index.js
+++ Server/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'http://localhost:3001',
     methods:['GET','POST','PUT','DELETE','PATCH'],
     credentials:true,
     allowedHeaders:['Content-Type','Authorization']
